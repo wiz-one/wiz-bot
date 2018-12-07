0e5c859981c47aae84c4cb91897c607c75e9dad3
darren96 <darren.oyk@gmail.com>
2018-12-07 14:43:21 +0800

    Add port listener to prevent heroku failed

--- bot.js
+++ bot.js
@@ -1,15 +1,22 @@
+const express = require('express');
 const Discord = require('discord.js');
+
+const { prefix, token } = require("./config.json");
+const PORT = process.env.PORT || 3000;
+
 const client = new Discord.Client();
+const server = express()
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('message', message => {
-  if (message.content === '!ping') {
+  if (message.content === `${prefix}ping`) {
     // send back "Pong." to the channel the message was sent in
     message.channel.send('Pong.');
   }
 });
 
-client.login('NTE5OTAyNzM4MzkwMzE5MTI2.DutbUw.GuJC9aRHgeO7W2xnggdMlCYRlo8');
+client.login(token);
