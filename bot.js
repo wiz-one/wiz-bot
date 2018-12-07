c9e85634d8c0b8bbd843f7e68a8f06e2089a2e27
darren96 <darren.oyk@gmail.com>
2018-12-07 23:53:15 +0800

    debug on deployment issue

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Discord = require('discord.js');
 
 const { prefix, token } = require("./config.json");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const client = new Discord.Client();
 const server = express()
