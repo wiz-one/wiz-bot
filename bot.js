26f9e4662c752e277bca40ccf211a54341922279
darren96 <darren.oyk@gmail.com>
2018-12-07 11:53:01 +0800

    Add dependencies into package.json

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 client.once('ready', () => {
     console.log('Ready!');
 });
+
 client.on('message', message => {
   if (message.content === '!ping') {
     // send back "Pong." to the channel the message was sent in
