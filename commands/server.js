78dd013ad5027a927b5ae1c4fb3217bd940e799e
LAPTOP-9O9BPBG3\CES <c_es@rocketmail.com>
2018-12-14 14:35:29 +0800

    Revert "Add some other commands for testing"
    This reverts commit e1a74c98a5470fa0c322437acf18e91de819545c.

--- commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'server',
-    description: 'Shows the server name and total number of members',
-    execute(message, args) {
-        message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    },
-}
--- commands/user-info.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'user-info',
-    description: 'Shows the username and id of user',
-    execute(message, args) {
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    },
-}
