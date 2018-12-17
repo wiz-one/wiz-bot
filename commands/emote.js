7d9969c49145b751d842453dc65e82e89ca719cd
darren96 <darren.oyk@gmail.com>
2018-12-17 16:26:14 +0800

    Able to handle both custom and standard emoji for emote

--- commands/emote.js
+++ commands/emote.js
@@ -1,9 +1,16 @@
+const emojis = require('emojis');
+const onlyEmoji = require('emoji-aware').onlyEmoji;
+const twemoji = require('twemoji');
+const Discord = require("discord.js");
+
 module.exports = {
   name: 'emote',
   description: 'Enlarge the emoji requested',
   async execute(message, args) {
-    const emojiUrl = "https://cdn.discordapp.com/emojis/";
+    const customEmojiUrl = "https://cdn.discordapp.com/emojis/";
+    const standardEmojiUrl = "https://s3.ap-southeast-1.amazonaws.com/wiz-bot/emoji/";
     const requestString = "Requested by: ";
+    var file = null;
 
     console.log("Argument received: ", args[0]);
 
@@ -11,20 +18,21 @@ module.exports = {
       return message.channel.reply("You didn't provide any arguments");
     }
 
-    console.log("\\" + args[0]);
+    if(onlyEmoji(args[0]).length !== 0){
+        var emojiUnicode = emojis.unicode(args[0]);
+        var emojiCodePoint = twemoji.convert.toCodePoint(emojiUnicode);
+        file = standardEmojiUrl + emojiCodePoint + ".png";
+        message.channel.send(requestString + message.author.username, { files: [file] });
+    } else {
+      const emoji = args[0].substr(2, args[0].length - 2).split(":");
+      const emojiId = emoji[1].slice(0,emoji[1].length-1);
 
-    const emoji = args[0].substring(2, args[0].length - 1).split(":");
-    console.log(emoji);
-
-    if (args.length == 0) {
-
+      console.log(message.guild.emojis.get(emojiId));
+      if (message.guild.emojis.get(emojiId)) {
+        file = customEmojiUrl + emojiId;
+        message.channel.send(requestString + message.author.username + "\n" + file);
+      }
     }
-    const emojiName = emoji[0];
-    const emojiId = emoji[1];
-
-    console.log(message.guild.emojis.get(emojiId)); 
-
-    message.channel.send(requestString + message.author.username + "\n" + emojiUrl + emojiId);
-    return message.delete();
+    message.delete();
   }
 }
