0fac6806c8e6f4a59c5ddf133cd79f4caa78fbfd
zulq9 <zul030995@gmail.com>
2018-12-20 22:57:44 +0800

    Improved command handler to handle subdirectories in command folder

---
+++ modules/getCommandFiles.js
@@ -0,0 +1,33 @@
+/** Returns a discord collection of commands,
+ *  with the command name as its indentifier and the command information as the body.
+ * 
+ *  dir: path of the directory you want to search the files for
+ *  fileTypes: array of file types you are search files, ex: ['.txt', '.jpg']
+ *
+ */
+
+const fs = require('fs');
+const path = require('path');
+const Discord = require('discord.js');
+
+module.exports = (client = Discord.Client) => {
+
+getCommandFiles = function getCommandFiles(dir, fileTypes) {
+    const cmd = new Discord.Collection();
+    function walkDir(currentPath) {
+      const files = fs.readdirSync(currentPath);
+      for (let i in files) {
+        const curFile = path.join(currentPath, files[i]);  
+        console.log(curFile);    
+        if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
+        const command = require(`./../${curFile.replace('\\','/')}`);
+        cmd.set(command.name, command);
+        } else if (fs.statSync(curFile).isDirectory()) {
+         walkDir(curFile);
+        }
+      }
+    };
+    walkDir(dir);
+    return cmd; 
+  };
+};
