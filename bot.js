7610db1bbb23d18c4567d00829384bf5c45db639
darren96 <darren.oyk@gmail.com>
2018-12-22 21:24:42 +0800

    Remove config.json

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Discord = require('discord.js');
 
-const { token } = require("./config.json");
+const { token } = process.env.token || require("./config.json");
 const PORT = process.env.PORT || 5000;
 
 const client = new Discord.Client();
