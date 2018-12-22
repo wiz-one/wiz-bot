9106ba67649a8ce2401c332ccebf71c1f0f03762
darren96 <darren.oyk@gmail.com>
2018-12-22 21:44:34 +0800

    Add config.json as an example to setup

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Discord = require('discord.js');
 
-const { token } = process.env.token || require("./config.json");
+var { token } = process.env || require("./config.json");
 const PORT = process.env.PORT || 5000;
 
 const client = new Discord.Client();
