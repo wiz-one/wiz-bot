c72454423467fa7b04132090f3a02c19d3606fb4
darren96 <darren.oyk@gmail.com>
2018-12-25 00:24:16 +0800

    Add modules needed for music commands

---
+++ modules/music.js
@@ -0,0 +1,112 @@
+const EventEmitter = require('events');
+const ytdl = require('ytdl-core');
+const Discord = require('discord.js');
+
+const streamOptions = { seek: 0, volume: 1 };
+
+let dispatcher;
+
+class Youtube extends EventEmitter {}
+
+youtube = new Youtube();
+
+youtube.on("play", (url, message) => {
+      if (!dispatcher) {
+        return module.exports.play(url, message);
+      }
+      module.exports.queue(url, message);
+    });
+
+module.exports = {
+  youtube: youtube,
+  play(song, message) {
+    const stream = ytdl(song, { quality: 'highest', filter : 'audioonly' });
+    ytdl.getBasicInfo(song)
+      .then((info) => {
+        var embedMessage = formEmbedMessage(message.author, info, true);
+        message.channel.send(embedMessage);
+        global.currentPlaying = { url:song, title:info.title, author:message.author }
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+    
+    dispatcher = message.guild.voiceConnection.playStream(stream, streamOptions);
+  
+    dispatcher.on('end', () => {
+      // The song has finished
+      if (global.playlist.length) {
+        nextSong = global.playlist.shift();
+        dispatcher = module.exports.play(nextSong.url, message);
+      } else {
+        global.currentPlaying = null;
+      }
+    });
+    
+    dispatcher.on('error', e => {
+      // Catch any errors that may arise
+      console.log(e);
+    });
+
+    return dispatcher;
+  },
+  queue(videoUrl, message) {
+    ytdl.getBasicInfo(videoUrl)
+      .then((info) => {
+        global.playlist.push({ url:videoUrl, title:info.title, author:message.author });
+        var embedMessage = formEmbedMessage(message.author, info, false);
+        message.channel.send(embedMessage);
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+    
+  },
+  queueTop(videoUrl, message) {
+    ytdl.getBasicInfo(videoUrl)
+      .then((info) => {
+        global.playlist.unshift({ url:videoUrl, title:info.title, author:message.author });
+        var embedMessage = formEmbedMessage(message.author, info, false);
+        message.channel.send(embedMessage);
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+  }
+}
+
+function formEmbedMessage(author, videoInfo, nowPlaying) {
+  var title = "";
+  var embedMessage = new Discord.RichEmbed();
+  if (nowPlaying) {
+    title = "Now Playing 🎵";
+  } else {
+    title = "Song Added 🎶";
+  }
+  length_seconds = videoInfo.length_seconds;
+  length = parseInt(length_seconds/60, 10) + ":" + ("0" + (length_seconds % 60)).slice(-2);
+  embedMessage
+    .setTitle(title)
+    .setDescription(`[${videoInfo.title}](${videoInfo.video_url})`)
+    .setColor('#da004e')
+    .setThumbnail(videoInfo.thumbnail_url)
+    .setAuthor(author.username, author.avatarURL, author.avatarURL)
+    .addField('Author: ', videoInfo.author.name)
+    .addField('Length: ', length)
+    .addField("Songs in Queue: ", global.playlist.length)
+    .setTimestamp();
+  
+  return embedMessage;
+}
+
+function sendInfo(ytdl, videoUrl, isNowPlaying, message) {
+  return ytdl.getBasicInfo(videoUrl)
+      .then((info) => {
+        var embedMessage = formEmbedMessage(message.author, info, isNowPlaying);
+        message.channel.send(embedMessage);
+        return info.title;
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+}
